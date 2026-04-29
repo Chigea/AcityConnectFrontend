94cfd1a23d13e16c7bc767bd8c66271c6b380a2c
Fidelia Chimezie <fideliachimezie000@gmail.com>
2026-04-29 19:51:45 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  const base = env.VITE_BASE_PATH || "/";
-
+  
   const proxyTarget = env.VITE_DEV_PROXY_TARGET;
   const proxy =
     proxyTarget && proxyTarget.startsWith("http")
